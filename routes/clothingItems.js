9054c1feb2e8c4540472f4f11dcea8bbe814c896
SamBelmonte <sambelmonte02@gmail.com>
2024-09-05 01:15:21 -0400

    Add like and dislike routes to clothingItems

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -10,5 +10,9 @@ router.post("/", () => console.log("POST new item"));
 router.delete("/:itemId", () => console.log("DELETE an item by Id"));
 
 //curl -X PUT http://localhost:3001/:itemId/likes
+router.put("/:itemId/likes", () => console.log("Like by an item by Id"));
+
+//curl -X DELETE http://localhost:3001/:itemId/likes
+router.delete("/:itemId/likes", () => console.log("Dislike by an item by Id"));
 
 module.exports = router;
